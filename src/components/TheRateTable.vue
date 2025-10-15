d02d8fe672893015b0fc4cfba35f39d8798bd5ab
marlon bagans <m.bagans@outlook.de>
2025-10-15 21:34:33 +0200

    fix script setup error

--- src/components/TheRateTable.vue
+++ src/components/TheRateTable.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import TableComponent from './TableComponent.vue';
+
+const tableTitle = ref('Bewertungen');
+const tableHeaders = ref(['Deck', '1', '2', '3', '4', '5', '6']);
+const tableRows = ref([]);
 </script>
 
 <template>
     <TableComponent :title="tableTitle" :headers="tableHeaders" :rows="tableRows" :rowsPerPage="3" :userColumns="[0]"/>
 </template>
-
-<script lang="ts">
-export default {
-    components: {
-        TableComponent
-    },
-    data() {
-        return {
-            tableTitle: 'Bewertungen',
-            tableHeaders: ['Deck', '1', '2', '3', '4', '5', '6'],
-            tableRows: [],
-        };
-    },
-}
-</script>
